08cfb6aa926104d7b1b9b61a6958999004606f47
Tisiqueira <tiago.m.siqueira@outlook.com>
2021-09-10 21:24:16 -0300

    "Criacao da nossa Home - aula 3"

---
+++ src/containers/Home.jsx
@@ -0,0 +1,25 @@
+import React,{ memo} from "react";
+import { Row, Col } from "antd";
+
+function Home(){
+    return (
+        <div>
+            <Row gutter={[16, 16]}>
+                <Col span={24} md={16}>
+                    <h2>World</h2>
+                </Col>
+                <Col span={24} md={16}>
+                    <h2>Economy</h2>
+                </Col>
+            </Row>
+            <Row gutter={[16, 16]}>
+                <Col span={24} md={16}>
+                    <h2>Tecnology</h2>
+                </Col>
+            </Row>
+        </div>
+    )
+    
+}
+
+export default Home;
